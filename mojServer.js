aea3a40362e22db94feca16ee868abd859713eac
Tomáš Ič <tomiic3@gmail.com>
2018-10-25 10:43:00 +0200

    nahratie

--- mojServer.js
+++ mojServer.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var path= reqire('path')
+var path= require('path')
 var server = express();
 var PORT = 3000;
 
@@ -15,6 +15,11 @@ server.get('/wms', function (request, response) {
     }
 
 })
+// http://localhost:3000/some/path?mojqueryparameter=super&service=wms&version=10
+server.get("/some/path", function(request, response) {
+    console.log(request.query);
+    response.send(request.query);
+
 server.listen(PORT, function() {
     console.log("Server listening on port " + PORT + "!");
   });
