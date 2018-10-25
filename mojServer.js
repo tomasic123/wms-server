118d1c8601d70fe4c3a11b6cbd417c9d92edb196
Tomáš Ič <tomiic3@gmail.com>
2018-10-25 11:32:41 +0200

    nahratie

--- mojServer.js
+++ mojServer.js
@@ -4,21 +4,17 @@ var server = express();
 var PORT = 3000;
 
 server.get('/wms', function (request, response) {
-    var params = reqest.query;
+    var params = request.query;
     console.log(params);
-    if(params.service === 'wms' && params.request === 'GetCapabilities'){
-        response.sendFile(path.join(__dirname, ' ./capa/wms_nase.xml'))
-    }else if (params.service === 'wms' && params.request === 'GetMap'){
+    if(params.SERVICE === 'WMS' && params.REQUEST === 'GetCapabilities'){
+        response.sendFile(path.join(__dirname, 'wms_nase.xml'))
+    }else if (params.SERVICE === 'WMS' && params.REQUEST === 'GetMap'){
         console.log('nejdeme robit get capa')
     }else {
         response.send('nepodporovana metoda')
     }
 
 })
-// http://localhost:3000/some/path?mojqueryparameter=super&service=wms&version=10
-server.get("/some/path", function(request, response) {
-    console.log(request.query);
-    response.send(request.query);
 
 server.listen(PORT, function() {
     console.log("Server listening on port " + PORT + "!");
