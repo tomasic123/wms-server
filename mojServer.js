3f57d08add4cac2b29e6ae405df746710ea6c381
Tomáš Ič <tomiic3@gmail.com>
2018-10-25 10:26:35 +0200

    pridal som xml dokument

--- mojServer.js
+++ mojServer.js
@@ -1,6 +1,21 @@
-var x = 5;
-var y = 6;
-var z = x + y;
+var express = require("express");
+var path= reqire('path')
+var server = express();
+var PORT = 3000;
 
-console.log(y);
+server.get('/wms', function (request, response) {
+    var params = reqest.query;
+    console.log(params);
+    if(params.service === 'wms' && params.request === 'GetCapabilities'){
+        response.sendFile(path.join(__dirname, ' ./capa/wms_nase.xml'))
+    }else if (params.service === 'wms' && params.request === 'GetMap'){
+        console.log('nejdeme robit get capa')
+    }else {
+        response.send('nepodporovana metoda')
+    }
+
+})
+server.listen(PORT, function() {
+    console.log("Server listening on port " + PORT + "!");
+  });
 
